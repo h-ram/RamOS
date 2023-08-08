eecb123d5ddc2176a189e86e13f7d483afad7641
lgriffithl <lliill.education.lliill@gmail.com>
2023-08-08 09:33:52 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -6,35 +6,52 @@ setInterval(()=>{
     dateWidget.textContent = new Date().toLocaleString().split(",")[1];
 },1000)
 
-//================ Dragging Windows =================
-const openWindow = document.querySelector(".window")
-const titleBar = document.querySelector(".window .titleBar")
-function moveWindow(event,barX,barY){
-    openWindow.style.left = (event.pageX-barX) + "px"
-    openWindow.style.top = (event.pageY-barY) + "px"
-}
-titleBar.addEventListener("mousedown",(event)=>{
-    const bar = titleBar.getBoundingClientRect();
-    var x = event.pageX-bar.left;
-    var y = event.pageY-bar.top;
-    function handleMove(event){
-            moveWindow(event,x,y)
-    }   
-    document.addEventListener("mousemove",handleMove)
-    titleBar.addEventListener("mouseup",()=>{
-        document.removeEventListener("mousemove",handleMove);
-    })
-})
-//================ Closing Windows =================
-const closeBtn = document.querySelector(".closeButton")
-closeBtn.addEventListener("click",()=>{
-    openWindow.style.animation = "closeWindow 1s 0s forwards"
-})
+
+
 //================ Opening Windows =================
 const executables = document.querySelectorAll(".exe")
 executables.forEach((exe) =>{
     exe.addEventListener("click",()=>{
-        console.log("openened")
-        openWindow.style.animation = "openWindow 1s 0s forwards"
+        const id = exe.id;
+        const currentWindow = document.querySelector(`#w-${id}`)
+        currentWindow.style.animation = "openWindow 1s 0s forwards"
+        exe.classList.add("isActive") 
+        //================ Closing Windows =================
+        const closeBtn = currentWindow.querySelector(`.closeButton`)
+        closeBtn.addEventListener("click",()=>{
+            currentWindow.style.animation = "closeWindow 1s 0s forwards"
+            exe.classList.remove("isActive")
+        })
+        //================ Dragging Windows =================
+        const titleBar = currentWindow.querySelector(".titleBar")
+        function moveWindow(event,barX,barY){
+            currentWindow.style.left = (event.pageX-barX) + "px"
+            currentWindow.style.top = (event.pageY-barY) + "px"
+        }
+        titleBar.addEventListener("mousedown",(event)=>{
+            const bar = titleBar.getBoundingClientRect();
+            var x = event.pageX-bar.left;
+            var y = event.pageY-bar.top;
+            function handleMove(event){
+                    moveWindow(event,x,y)
+            }   
+            document.addEventListener("mousemove",handleMove)
+            titleBar.addEventListener("mouseup",()=>{
+                document.removeEventListener("mousemove",handleMove);
+            })
+        })
+        //================ Refocusing Windows =================
+        // var topWindow = 2;
+        // currentWindow.addEventListener("mousedown",()=>{
+        //     if(currentWindow.style.zIndex >= topWindow) return
+        //     currentWindow.style.zIndex = topWindow+1;
+        //     topWindow++;
+        //     console.log("jamp")
+        // })
+        //================ Minimizing Windows =================
+        const hideBtn = currentWindow.querySelector(`.hideButton`)
+        hideBtn.addEventListener("click",()=>{
+            currentWindow.style.animation = "closeWindow 1s 0s forwards"
+        })
     })
 })
